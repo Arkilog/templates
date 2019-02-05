4e9a92a2c36d502d8b21e4a5fc4f7966c35be3a9
yellowsnow <contact@arkilog.ma>
2019-02-05 10:06:48 +0100

    Delete run-it.sh

--- run-it.sh
+++
@@ -1,4 +0,0 @@
-eval "$(cat /etc/environment | xargs printf "export %s\n")"
-eval "$(AWS_DEFAULT_REGION=$APP_REGION aws ecr get-login --no-include-email)"
-docker pull $APP_IMAGE_URL
-(echo cd /home/ec2-user;(cat /etc/environment | xargs printf "%s ");echo $run_svc) > ./run-docker-compose && chmod +x ./run-docker-compose
